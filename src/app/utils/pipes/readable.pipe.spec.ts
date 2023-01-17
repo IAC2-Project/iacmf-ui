a7276e84010b808e108f42c6c13750cfc43963bd
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2023-01-17 15:46:40 +0100

    Delete readable.pipe.spec.ts

--- src/app/utils/pipes/readable.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ReadablePipe } from './readable.pipe';
-
-describe('ReadablePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ReadablePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
